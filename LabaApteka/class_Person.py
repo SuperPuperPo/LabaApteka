90a3ff2f149d46fdcd0aa88d75d01cc59b99ce71
Данил Платонов <Danil.Platonov@inducyber.com>
2022-05-23 13:07:47 +0500

    Добавлена документация

--- LabaApteka/class_Person.py
+++ LabaApteka/class_Person.py
@@ -1,15 +1,51 @@
 # -*- coding: cp1251 -*-
 class Person(object):
+    """
+    Класс Покупатель.
+    
+    Атрибуты:
+
+    'name': **Строка** с именем пользователя
+
+    'phnoe_number': **Число** с номером телефона пользователя
+
+    'order':*массив* с объектами класса **Order**
+    
+    """
     def __init__(self):
+        """
+        Конструктор создает пустого покупателя.
+        """
         self.name=""
         self.phnoe_number=""
         self.order=[]
 
     def add_order(self,order):
+        """
+        Метод для добавления заказа.
+        
+        Аргументы:
+
+        'order' : Объект класса **Order**
+        """
         self.order=self.order+[order]
 
     def add_phnoe_number(self,phone_number):
+        """
+        Метод для добавления номера телефона.
+        
+        Аргументы:
+
+        'phone_number' : **Число** с номером телефона пользователя        
+        """
         self.phnoe_number=phone_number
 
     def add_Name(self,name):
+        """
+        Метод для добваления имени покупателя.
+        
+        Аргументы:
+
+        'name' :**Строка** с именем покупателя        
+        """
         self.name=name
